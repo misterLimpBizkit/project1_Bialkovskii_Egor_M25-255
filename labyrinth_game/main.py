8144a2e374ae8b21c82378c4396ff1548932daac
MisterSigmaMac <egorzapekanka@gmail.com>
2025-10-23 14:54:08 +0300

    Реализованы основные вводные данные игры

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -4,3 +4,11 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+game_state = {
+    'player_inventory': [], # Инвентарь игрока
+    'current_room': 'entrance', # Текущая комната
+    'game_over': False, # Значения окончания игры
+    'steps_taken': 0 # Количество шагов
+}
